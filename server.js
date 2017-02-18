3fa9e960dcb84e8c3e31f0a2e7d3a1c9065ed304
Ayabonga <aya@westerncapelabs.com>
2017-02-18 11:46:08 +0200

    Added projects and developers routes

---
+++ server.js
@@ -0,0 +1,34 @@
+express = require('express')
+var exphbs = require('express-handlebars');
+var app = express()
+
+
+
+var gatRoutes = require('./projects/GetMeThere/app.js')
+app.use('/projects/gatjie', gatRoutes);
+gatRoutes._router.stack.forEach(function(r){
+  if (r.route && r.route.path){
+    r.route.path = '/projects/gatjie'+r.route.path;
+  }
+})
+var devProfile = require('./developers/ayabonga/app.js')
+app.use('/developers/ayabonga', devProfile);
+devProfile._router.stack.forEach(function(r){
+  if (r.route && r.route.path){
+    r.route.path = '/developers/ayabonga'+r.route.path;
+  }
+})
+
+
+app.use(express.static( __dirname+'/public'))
+app.engine('handlebars', exphbs({defaultLayout: 'main',layoutsDir:__dirname+'/views/layouts'}));
+app.set('view engine', 'handlebars');
+app.set('views', __dirname+'/views');
+
+app.get('/',function(req,res){
+
+  res.sendfile('index.html')
+
+})
+
+app.listen(5000)
